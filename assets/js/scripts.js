893555697d0818f6bbe683337abeb719bdcc5672
Kim <kimtuan.doan@gmail.com>
2021-11-24 21:25:26 +0100

    index: dynamic javascript for popup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,11 @@
+/**
+ * Fonctions pour le top header: burger menu et barre de navigation
+ */
+
+// Listener sur la div contenant les icones
+document.getElementById("burgermenu").addEventListener("click", toggleSideNav);
+
+// évite la répétition de code
 function reverseDisplay(x) {
     if (x.style.display == "none") {
         x.style.display = "block";
@@ -6,15 +14,7 @@ function reverseDisplay(x) {
     }
 }
 
-function toggleDiv(element) {
-    let _element_css = window.getComputedStyle(element);
-    if (_element_css.height == "0px") {
-        element.style.height = "20px";
-    } else {
-        element.style.height = "0px";
-    }
-}
-
+// burger icon <-> close icon
 function switchIcons() {
     let menu = document.getElementById("nav-menu-icon");
     let close = document.getElementById("nav-close-icon");
@@ -22,17 +22,23 @@ function switchIcons() {
     reverseDisplay(close);
 }
 
+// show/hide nav
 function toggleSideNav() {
-    let x = document.getElementById("nav-side");
-    toggleDiv(x);
+    let element = document.getElementById("nav-side");
+    let _element_css = window.getComputedStyle(element);
+    if (_element_css.height == "0px") {
+        element.style.height = "20px";
+    } else {
+        element.style.height = "0px";
+    }
     switchIcons();
 }
-
-document.getElementById("burgermenu").addEventListener("click", toggleSideNav);
 
 /**
  * Go Top icon
  */
+
+// Listeners
 window.addEventListener("scroll", showGoToTopIcon);
 document.getElementById("go_to_top").addEventListener("click", scrollToTop);
 
@@ -61,12 +67,20 @@ function showConfirmation(e) {
 
     let _idText = e.target.id == "form_contact" ? "confirmation_message_text" : "confirmation_newsletter_text";
 
-    // Calcul de la hauteur totale de notre popup
+    // Calcul de la valeur de translation de notre popup
     // hauteur totale = height + border-top + border-bottom
-    // let _height = 
-    // parseCssToInt(window.getComputedStyle(confirmation).getPropertyValue("height"))
-    // + parseCssToInt(window.getComputedStyle(confirmation).getPropertyValue("border-top-width")) 
-    // + parseCssToInt(window.getComputedStyle(confirmation).getPropertyValue("border-bottom-width"));
+    // reconverti en string pour le css
+    let _height =
+        "-"
+        +
+        (
+            parseWidthToInt(window.getComputedStyle(confirmation).getPropertyValue("height"))
+            + parseWidthToInt(window.getComputedStyle(confirmation).getPropertyValue("border-top-width"))
+            + parseWidthToInt(window.getComputedStyle(confirmation).getPropertyValue("border-bottom-width"))
+        )
+        +
+        "px";
+    // console.log(_height);
 
     // Pop Up
     document.getElementById("background_overlay").style.display = "block";
@@ -75,7 +89,7 @@ function showConfirmation(e) {
 
     // Pop out
     setTimeout(() => {
-        document.getElementById("confirmation").style.bottom = "-210px";
+        document.getElementById("confirmation").style.bottom = _height;
         document.getElementById(_idText).style.display = "none";
         document.getElementById("background_overlay").style.display = "none";
     }, 3000);
@@ -83,6 +97,19 @@ function showConfirmation(e) {
 
 
 // Cette fonction est nécessaire pour récupérer la valeur entière d'une propriété CSS de type taille d'unité px/em/rem
-function parseCssToInt() {
-    
+// La valeur est arrondie à l'entier supérieur pour éviter tout débordement de pixels liés aux tailles de type float
+// Compléxité O(n)
+function parseWidthToInt(stringSize) {
+    // console.log(stringSize);
+    let _j = "";
+    for (let i = 0; i < stringSize.length; i++) {
+        if (stringSize[i] == 'p') {
+            break;
+        }
+        else {
+            _j += stringSize[i];
+        }
+    }
+    // console.log(Math.ceil(_j));
+    return Math.ceil(_j);
 }
